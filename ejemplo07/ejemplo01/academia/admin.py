5bca6125958e4e53c08dd1d5bd3de2743128e7e1
JU4NSCV <jscueva5@utpl.edu.ec>
2026-06-11 12:52:51 -0500

    Actualizacion

--- ejemplo07/ejemplo01/academia/admin.py
+++ ejemplo07/ejemplo01/academia/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-admin.site.register(Estudiante)
+class EstudianteAdmin(admin.ModelAdmin):
+    # listado de atributos que se mostrará
+    # por cada registro
+    # se deja de usar la representación (str)
+    # de la clase
+    list_display = ('nombre', 'apellido', 'cedula', 'edad',
+        'comprobar_provincia', 'calcular_anio_nacimiento')
+    search_fields = ('nombre', 'cedula', 'apellido')
+
+
+admin.site.register(Estudiante, EstudianteAdmin)
